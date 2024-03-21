ddd363048275bb84705b32b40f4686fd1f6000aa
seftontycho <seftontycho@hotmail.co.uk>
2024-03-21 15:26:31 +0000

    update tests

--- tests/broadcast/array1_scalar.rs
+++ tests/broadcast/array1_scalar.rs
@@ -1,11 +1,10 @@
-use matrix::broadcast::*;
-use matrix::prelude::Array1;
+use matrix::prelude::*;
 
 fn main() {
     let a = Array1::<i32, 3>::from([1, 2, 3]);
     let b = 2;
 
-    let c = a.broadcast(b, |&a, &b| a * b);
+    let c = a.broadcast(b, |a, b| a * b);
 
     assert_eq!(c, Array1::from([2, 4, 6]));
 }
